9e06e558a3d2c9dce598a09f904754d95136b5ff
wangy <wangyunze412@gmail.com>
2021-04-04 12:12:00 +0800

    :bug: Fix pawn move generation bug

--- shared/chess/move-generator.ts
+++ shared/chess/move-generator.ts
@@ -1,9 +1,9 @@
-import { NUM_FILES, NUM_RANKS } from "../constants";
+import {NUM_FILES, NUM_RANKS} from "../constants";
 import Pair from "../ds/pair";
 import multipleEquals from "../utilities/multiple-equals";
-import { PieceGrid } from "./board";
+import {PieceGrid} from "./board";
 import Move from "./move";
-import { PieceSide, PieceType } from "./piece";
+import {PieceSide, PieceType} from "./piece";
 
 // IMPORTANT: This only generates pseudo legal moves
 // aka it can allow itself to die when being checked
@@ -234,7 +234,7 @@ function pawnGenerator(position: Pair, board: PieceGrid): Move[] {
   } else {
     const nx = position.first + 1 * (piece.side == PieceSide.Red ? 1 : -1);
     const ny = position.second;
-    if (board[nx][ny]) {
+    if (board[nx][ny]?.side == piece.side) {
       return [];
     } else {
       return [new Move(position, new Pair(nx, ny))];
@@ -242,7 +242,7 @@ function pawnGenerator(position: Pair, board: PieceGrid): Move[] {
   }
 }
 
-export const MOVE_GENERATORS: { [k in PieceType]: MoveGenerator } = {
+export const MOVE_GENERATORS: {[k in PieceType]: MoveGenerator} = {
   Rook: rookGenerator,
   Horse: horseGenerator,
   Elephant: elephantGenerator,
