9864004e1481d93adbc86123bcc780f1238afbdf
wangy <wangyunze412@gmail.com>
2021-05-03 23:07:33 +0800

    Try fix cors againnnn

--- src/client/socket.ts
+++ src/client/socket.ts
@@ -1,4 +1,9 @@
 import { io } from "socket.io-client";
 
 // TODO: Change to use __SERVER_URL__
-export default io("http://localhost:3000", { autoConnect: false });
+export default io(
+  process.env.NODE_ENV == "production"
+    ? "xiangqi.yunze.wang"
+    : "http://localhost:3000",
+  { autoConnect: false }
+);
