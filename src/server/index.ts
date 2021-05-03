c60634b3285a7a69113f9701171df5930d78f8df
wangy <wangyunze412@gmail.com>
2021-05-03 23:00:05 +0800

    try to fix cors again

--- src/server/index.ts
+++ src/server/index.ts
@@ -14,7 +14,10 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080",
+    origin:
+      process.env.NODE_ENV == "production"
+        ? "https://xiangqi.yunze.wang"
+        : "http://localhost:8080",
     methods: ["GET", "POST"],
   },
 });
